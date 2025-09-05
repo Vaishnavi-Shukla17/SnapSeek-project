1b818b543124d4694d2ad71f94ad41b59a914642
Vaibhav Pandey <vaibhavpandey098@gmail.com>
2025-09-05 12:08:55 +0530

    initial

--- src/main/java/com/SnapSeek/Main.java
+++ src/main/java/com/SnapSeek/Main.java
@@ -1,7 +1,32 @@
 package com.SnapSeek;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        URL fxmlUrl = Main.class.getResource("/com.SnapSeek/View.fxml");
+        System.out.println(fxmlUrl);  // Should print something like: file:/C:/.../target/classes/com/SnapSeek/View.fxml
+        FXMLLoader loader = new FXMLLoader(fxmlUrl);
+        Parent root = loader.load();
+
+         loader = new FXMLLoader(Main.class.getResource("/com.SnapSeek/View.fxml"));
+        root = loader.load();
+
+        Scene scene = new Scene(root, 800, 600);
+        primaryStage.setTitle("SnapSeek - AI Image Search");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello SnapSeek");
+        launch(args);
     }
 }
--- src/main/java/com/SnapSeek/MainController.java
+++ src/main/java/com/SnapSeek/MainController.java
@@ -1,4 +1,84 @@
 package com.SnapSeek;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
 public class MainController {
+
+    // UI components
+    @FXML
+    private Button folderButton;    // "Select Folder" button
+    @FXML
+    private Button searchButton;    // Search button
+    @FXML
+    private TextField searchField;  // Search query input
+    @FXML
+    private FlowPane resultsPane;   //to display image results
+
+    // Data
+    private File selectedFolder;
+
+    @FXML
+    public void handleSelectFolder(ActionEvent actionEvent) {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Select Image Folder");
+
+        // Start from user's home directory
+        directoryChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+
+        // Open dialog
+        File folder = directoryChooser.showDialog(new Stage());
+        if (folder != null && folder.isDirectory()) {
+            selectedFolder = folder;
+            System.out.println("Selected Folder: " + selectedFolder.getAbsolutePath());
+        } else {
+            System.out.println("No folder selected");
+        }
+    }
+
+    @FXML
+    public void handleSearch(ActionEvent event) {
+        if (selectedFolder == null) {
+            System.out.println("Please select a folder first!");
+            return;
+        }
+
+        String query = searchField.getText().trim();
+        if (query.isEmpty()) {
+            System.out.println("Please enter a search query");
+            return;
+        }
+
+        // Clear previous results
+        resultsPane.getChildren().clear();
+
+        // Add dummy results
+        for (int i = 1; i <= 6; i++) {
+            Region placeholder = new Region();
+            placeholder.setPrefSize(100, 100);
+            placeholder.setStyle("-fx-background-color: lightgray;");
+
+            Label caption = new Label("Image " + i);
+
+            VBox card = new VBox(5);
+            card.setAlignment(Pos.CENTER);
+            card.getChildren().addAll(placeholder, caption);
+
+            resultsPane.getChildren().add(card);
+        }
+
+        System.out.println("Dummy search performed for query: " + query);
+    }
+
 }
